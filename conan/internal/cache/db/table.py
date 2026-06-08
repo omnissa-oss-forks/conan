70d1d24799753144c1ca40a08ce192df0d6bd762
James Benton <bentonj@omnissa.com>
2026-06-08 14:59:50 +0100

    Set timeout to 120s

--- conan/internal/cache/db/table.py
+++ conan/internal/cache/db/table.py
@@ -27,10 +27,10 @@ class BaseDbTable:
 
     @contextmanager
     def db_connection(self):
-        if not self._lock.acquire(timeout=20):
+        if not self._lock.acquire(timeout=120):
             m = "".join(traceback.format_stack()) + "\n"
             ConanOutput().error("Error while acquiring lock for DB: " + m)
-            raise ConanException("Conan failed to acquire database lock in 20s. Maybe the system is "
+            raise ConanException("Conan failed to acquire database lock in 60s. Maybe the system is "
                                  "under very heavy load. Please report it to Github tickets")
         # isolation_level=None, puts it in regular SQLITE autocommit mode, every
         # connection.execute() will autocommit
